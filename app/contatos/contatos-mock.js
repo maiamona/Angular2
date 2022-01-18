49723c9453e16f906eeee8909001fad7b1077b04
mona <manomona2@hotmail.com>
2022-01-18 14:29:00 +0100

    6

--- app/contatos/contatos-mock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.CONTATOS = [
-    { id: 1, nome: 'Emanuel', email: 'emanuelcazevo@gmail.com', telefone: '000000000' },
-    { id: 2, nome: 'Maiamona', email: 'manomona2@hotmail.com', telefone: '923744720' },
-    { id: 3, nome: 'Mixa', email: 'miguelcazevo@gmail.com', telefone: '111111111' },
-    { id: 4, nome: 'Lucy', email: 'lucindacazevo@gmail.com', telefone: '222222222' },
-    { id: 5, nome: 'Jane', email: 'juremagarcia@gmail.com', telefone: '333333333' },
-];
-//# sourceMappingURL=contatos-mock.js.map
